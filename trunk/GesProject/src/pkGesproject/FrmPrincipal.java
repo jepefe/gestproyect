88527edb0ed135c0b3b314fd5ccd6793b867573e
jepefe <jepefe@46d8371f-59be-29b9-8b59-188e4420dafa>
2010-11-14 12:05:14 +0000

    Añado ActionListener para poder cambiar de "pestaña" en la JToolBar, añado un dialogo de prueba que aun no funciona fino, este se activa al pulsar el boton con icono de usuario.

--- trunk/GesProject/src/pkGesproject/FrmPrincipal.java
+++ trunk/GesProject/src/pkGesproject/FrmPrincipal.java
@@ -5,13 +5,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.Rectangle;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.UIManager;
@@ -19,7 +19,6 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.Border;
 
 import com.seaglasslookandfeel.SeaGlassLookAndFeel;
-import com.seaglasslookandfeel.effect.*;
 import com.seaglasslookandfeel.ui.SeaGlassRootPaneUI;
 /**
  * Clase de la ventana principal de la aplicación
@@ -30,17 +29,20 @@ public class FrmPrincipal extends JFrame {
 
 	JToolBar jtlbFrmppal = new JToolBar();
 	RsGesproject recursos = RsGesproject.Obtener_Instancia();
-	//Dimension tamanyoVentana = Toolkit.getDefaultToolkit().getScreenSize();
+	JToggleButton jbtnUsuarios = new JToggleButton(recursos.icono[0]);
+	JToggleButton jbtnSocios = new JToggleButton(recursos.icono[1]);
+	PnlAltasocio nu = new PnlAltasocio();
 	private Rectangle maxBounds;
+	
 	public FrmPrincipal(){
 		super.setBounds(0, 0, recursos.getFrmppalWidth(), recursos.getFrmppalHeight());
 		this.setTitle(recursos.APNAME);
 		recursos.setRfrmppal(this);	
 		this.setLayout(new BorderLayout());
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.inicializar();
-		PnlAltasocio nu = new PnlAltasocio();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.add(nu,BorderLayout.CENTER);
+		this.setVisible(true);
+		this.inicializar();
 	}
 	
 	/**
@@ -60,19 +62,41 @@ public class FrmPrincipal extends JFrame {
 	 * Metodo de carga de la barra de herramientas
 	 */
 	public void Carga_toolbar(){
-		JToggleButton jbtnUsuarios = new JToggleButton(recursos.icono[0]);
-		JToggleButton jbtnSocios = new JToggleButton(recursos.icono[1]);
+		//Creamos un action listener para los botones de la jtoolbar
+		ActionListener jtlbAcListener = new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	        	jbtnUsuarios.setSelected(false);
+	        	jbtnSocios.setSelected(false);
+	        	nu.setVisible(false);
+	         if (e.getActionCommand().equals("socios")){
+	        	jbtnSocios.setSelected(true); 
+	        	nu.setVisible(true);
+	        	
+	        	
+	        	}
+	         if (e.getActionCommand().equals("usuarios")){
+		        	jbtnUsuarios.setSelected(true); 
+		        new DlgSiNo("Seguro que desea salir?").start();
+		        
+		         }
+	         } 
+		};
+		jbtnUsuarios.addActionListener(jtlbAcListener);
+		jbtnSocios.addActionListener(jtlbAcListener);
+		jbtnUsuarios.setActionCommand("usuarios");
+		jbtnSocios.setActionCommand("socios");
 		jtlbFrmppal.setFloatable(false); //Impedimos que el toolbar se pueda separar de la ventana
 		jtlbFrmppal.setPreferredSize(new Dimension(this.getWidth(),70));
 		jtlbFrmppal.setVisible(true);
 		jtlbFrmppal.add(jbtnUsuarios);
 		//jbtnSocios.addActionListener();
-
 		jtlbFrmppal.add(jbtnSocios);
-		//jbtnSocios.setSelected(true);
-		
 		this.add(jtlbFrmppal,BorderLayout.NORTH);
+		
+
 	}
+	
+
 	
 	/**
 	 * Metodo necesario para que un JFrame sin decoracion tenga en cuenta la taskbar del sistema al maximizar, sobreescribe a un metodo de super()
@@ -108,6 +132,8 @@ public class FrmPrincipal extends JFrame {
 		this.maxBounds = maxBounds;
 		super.setMaximizedBounds(maxBounds);
 	}
+
+
 
 	
 }
