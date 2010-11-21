dbba086d7b264684db8827e2311884f4a74708f1
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2010-11-21 17:44:35 +0000

    Mejorado spnstaff

--- GesProject/src/pkGesproject/SpnStaff.java
+++ GesProject/src/pkGesproject/SpnStaff.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.JPanel;
@@ -21,6 +23,8 @@ public class SpnStaff extends JSplitPane{
 	Vector botones = new Vector(3,2);
 	JPanel panel = new JPanel();
 	pnlAlta_staff pnlalta_staff = new pnlAlta_staff();
+	PnlAltasocio pnlaltasocio = new PnlAltasocio();
+	PnlNuevoProyecto pnlnuevoproyecto = new PnlNuevoProyecto();
 	int i;
 	//JToggleButton bt = new JToggleButton(recursos.icono[5]);
 	
@@ -42,26 +46,57 @@ public class SpnStaff extends JSplitPane{
     	//gbc.fill = GridBagConstraints.WEST;
     	
     	
-    	JToggleButton bt = new JToggleButton(recursos.icono[5]);
+    	final JToggleButton bt = new JToggleButton(recursos.icono[5]);
     	bt.setText(rec.idioma[rec.eleidioma][13]);
+    	bt.setActionCommand("alta");
+    	
     	jtlbLateral.add(bt,gbc);
     	
-    	JToggleButton bt1 = new JToggleButton(recursos.icono[6]);
+    	final JToggleButton bt1 = new JToggleButton(recursos.icono[6]);
     	bt1.setText(rec.idioma[rec.eleidioma][14]);
+    	bt1.setActionCommand("modificacion");
     	jtlbLateral.add(bt1,gbc);
     	
-    	JToggleButton bt2 = new JToggleButton(recursos.icono[7]);
+    	final JToggleButton bt2 = new JToggleButton(recursos.icono[7]);
     	bt2.setText(rec.idioma[rec.eleidioma][15]);
+    	bt2.setActionCommand("mostrar");
     	jtlbLateral.add(bt2,gbc);
     	
-		/*
-    	jsplpane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jtlbLateral, prueba);
-    	jsplpane.setOneTouchExpandable(true);
-    	jsplpane.setOpaque(true);
-    	*/
     	
+    	ActionListener jtlbactionlistener = new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				bt.setSelected(false);
+				bt1.setSelected(false);
+				bt2.setSelected(false);
+				remove(pnlalta_staff);
+				remove(pnlaltasocio);
+				remove(pnlnuevoproyecto);
+				if(e.getActionCommand().equals("alta")){
+					bt.setSelected(true);
+					setRightComponent(pnlalta_staff);
+				}
+				
+				if(e.getActionCommand().equals("modificacion")){
+					bt1.setSelected(true);
+					setRightComponent(pnlaltasocio);
+				}
+				
+				if(e.getActionCommand().equals("mostrar")){
+					bt2.setSelected(true);
+					setRightComponent(pnlnuevoproyecto);
+				}
+			}
+    		
+    	};
     	
+    	bt.addActionListener(jtlbactionlistener);
+    	bt1.addActionListener(jtlbactionlistener);
+    	bt2.addActionListener(jtlbactionlistener);
+    
     	this.setLeftComponent(jtlbLateral);
-    	this.setRightComponent(pnlalta_staff);
+    	this.setRightComponent(panel);
 	}
 }
