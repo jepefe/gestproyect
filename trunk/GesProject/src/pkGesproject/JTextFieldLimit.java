f5e61a10065e4cf8061796f06b53c04653971a68
bernabe.r.v@gmail.com <bernabe.r.v@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2010-11-23 12:47:57 +0000

    Nueva Clase Limite de caracteres. Probando en Panel Nuevo proyecto.

---
+++ trunk/GesProject/src/pkGesproject/JTextFieldLimit.java
@@ -0,0 +1,34 @@
+package pkGesproject;
+import javax.swing.text.*; 
+
+/**
+ * Clase para limitar los JTextField, solo hay que introducir en el Jtext field new JTextFieldLimit(numero).
+ */
+
+public class JTextFieldLimit extends PlainDocument { 
+    private int limit; 
+
+    private boolean toUppercase = false ; 
+    
+    JTextFieldLimit ( int limit ) { 
+        super () ; 
+        this .limit = limit; 
+    } 
+    
+    JTextFieldLimit ( int limit, boolean upper ) { 
+        super () ; 
+        this .limit = limit; 
+        toUppercase = upper; 
+    } 
+    
+    public void insertString 
+            ( int offset, String  str, AttributeSet attr ) 
+            throws BadLocationException { 
+        if ( str == null ) return ; 
+        
+        if (( getLength () + str.length ()) <= limit ) { 
+            if ( toUppercase ) str = str.toUpperCase () ; 
+            super .insertString ( offset, str, attr ) ; 
+        } 
+    } 
+} 
