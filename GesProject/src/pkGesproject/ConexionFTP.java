c8d66211c19d0da05de303cdf0319d0db8c7f7ec
jepefe <jepefe@46d8371f-59be-29b9-8b59-188e4420dafa>
2010-11-28 04:59:14 +0000

    Empiezo con la clase de gestion de conexiones ftp

---
+++ GesProject/src/pkGesproject/ConexionFTP.java
@@ -0,0 +1,19 @@
+package pkGesproject;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+public class ConexionFTP {
+	
+	
+
+}
