c9b188baf9af70146594cba9b431ef484e394263
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2010-12-21 22:21:37 +0000

    Visible agregar palabras

--- GesProject/src/pkGesproject/FrmPrincipal.java
+++ GesProject/src/pkGesproject/FrmPrincipal.java
@@ -63,7 +63,7 @@ public class FrmPrincipal extends JFrame {
 	private Rectangle maxBounds;
 	
 	public FrmPrincipal(){
-		jbtnAgregar.setVisible(false);
+		jbtnAgregar.setVisible(true);
 		super.setBounds(0, 0, recursos.getFrmppalWidth(), recursos.getFrmppalHeight());
 		this.setTitle(recursos.APNAME);
 		this.setLocationRelativeTo(null);
