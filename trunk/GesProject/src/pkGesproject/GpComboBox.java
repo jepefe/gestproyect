201ae35cd1142ed56e10c8dc20592fe56c8bbea3
jepefe <jepefe@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-12 00:05:18 +0000

    Añado el codigo fuente del Look And Feel completo para facilitar la tarea de solucionar posibles futuros bugs y añado  clase GpComboBox que debe sustituir a JComboBox

---
+++ trunk/GesProject/src/pkGesproject/GpComboBox.java
@@ -0,0 +1,17 @@
+package pkGesproject;
+
+import javax.swing.JComboBox;
+/**
+ * 
+ * @author Jesus Perez
+ * Clase que hereda de JComboBox y utiliza Estilo modificado por defecto
+ *
+ */
+
+public class GpComboBox extends JComboBox{
+	
+		  public GpComboBox ()
+		    {
+		    setUI( new EstiloComboBox() );
+		    }
+}
