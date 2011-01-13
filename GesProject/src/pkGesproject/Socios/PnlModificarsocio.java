93759b594863c949e8c3e557634a508338c81cd0
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-13 23:38:52 +0000

    Solucionados bug's busqueda socio y staff

--- GesProject/src/pkGesproject/Socios/PnlModificarsocio.java
+++ GesProject/src/pkGesproject/Socios/PnlModificarsocio.java
@@ -6,6 +6,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.sql.ResultSet;
@@ -96,9 +98,28 @@ public class PnlModificarsocio extends JPanel{
         	jtblLateral  = new JTable(tablemodel= new DefaultTableModel(datos,colu));
        
     		jtxt=new JTextField(20);
-    		//jtxt.setText("Buscar...");
+    		jtxt.setText("Buscar...");
     		jtxt.putClientProperty("JTextField.variant", "search");
     		jtxt.putClientProperty("JTextField.Search.PlaceholderText", Boolean.TRUE);
+    		jtxt.addFocusListener(new FocusListener(){
+
+				@Override
+				public void focusGained(FocusEvent arg0) {
+					// TODO Auto-generated method stub
+					jtxt.setText("");
+				}
+
+				@Override
+				public void focusLost(FocusEvent arg0) {
+					// TODO Auto-generated method stub
+					if(jtxt.getText().equals("")){
+						jtxt.setText("Buscar...");
+					}
+					
+				}
+    			
+    		});
+    		
     		
             GridBagConstraints constraints = new GridBagConstraints();
             constraints.gridx = 0; // El área de texto empieza en la columna
