3ae7cd8e73ad959faaa3f0dd0a29fde901cec262
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-16 20:03:55 +0000

    Solucionado actualización modificarsocio al eliminar un socio

--- trunk/GesProject/src/pkGesproject/Socios/PnlModificarsocio.java
+++ trunk/GesProject/src/pkGesproject/Socios/PnlModificarsocio.java
@@ -28,6 +28,11 @@ import pkGesproject.GesIdioma;
 import pkGesproject.RsGesproject;
 import pkGesproject.Staff.pnlAlta_staff;
 
+/**
+ * Esta clase esta creada para listar todos los partners, poder modificar los datos de cualquiera de ellos
+ * y tambien la opción de elimiar alguno.
+ **/
+
 public class PnlModificarsocio extends JPanel{
 	RsGesproject recursos = RsGesproject.Obtener_Instancia();
 	GesIdioma rec = GesIdioma.obtener_instancia();
@@ -35,6 +40,7 @@ public class PnlModificarsocio extends JPanel{
 	ResultSet rs,rs2;
 	JPanel panel = new JPanel();
 	int cuenta = 0;
+	int columnas;
 	String datos[][];
 	String auxdatos[][];
 	String colu[] = {"Nombre","Telefono","Telefono 2"};
@@ -46,14 +52,15 @@ public class PnlModificarsocio extends JPanel{
 	JTextField jtxt;
 	JButton jbtn,jbtnmodificar,jbtneliminar,jbtnaceptar,jbtncancelar;
 	Boolean llena = new Boolean(false);
+	Boolean existe= new Boolean(false);
 	String[] fila = new String[3];
 	JTable jtblLateral;
 	JScrollPane jspntabla;
-    
+	static PnlModificarsocio instancia = new PnlModificarsocio(); 
 	DefaultTableModel tablemodel = new DefaultTableModel(null,colu); //Creamos el tablemodel global y le pasamos las columnas
     
     public PnlModificarsocio(){
-    	
+    	/*
     	String resul[]= new String[3];
     	conexion.Conectardb();
     	//rs = conexion.ConsultaSQL("SELECT COUNT(*) FROM STAFF");
@@ -87,16 +94,22 @@ public class PnlModificarsocio extends JPanel{
 			e.printStackTrace();
 		}
     	conexion.cerrarConexion();
+    	*/
     	
-    		
+    	
+    	
     	this.setLayout(new GridBagLayout()); //Ponemos el Layout al panel
 		   
-    	
-    	
-    	
-		    
-        	jtblLateral  = new JTable(tablemodel= new DefaultTableModel(datos,colu));
-       
+    	cuenta=contar_reg();
+    	columnas =3;
+    	datos = new String[cuenta][columnas];
+		auxdatos = new String[cuenta][columnas];
+    	tablemodel=cargar_tabla(datos);
+    	jtblLateral  = new JTable(tablemodel);
+        	
+        	/**
+        	 * Creamos el jtextfield para la busquera y le añadimos un focus listener para que se borre su contenido
+        	 **/
     		jtxt=new JTextField(20);
     		jtxt.setText("Buscar...");
     		jtxt.putClientProperty("JTextField.variant", "search");
@@ -178,90 +191,70 @@ public class PnlModificarsocio extends JPanel{
     			public void keyReleased(KeyEvent act) {
     				// TODO Auto-generated method stub
     				
-    				
     				if(llena==false){
     					for(int i=0;i<cuenta;i++){
-    						for(int j = 0;j<3;j++){
+    						for(int j = 0;j<columnas;j++){
     							auxdatos[i][j]= datos[i][j];
-    							datos[i][j]=null;
-    							//System.out.print(auxdatos[i][j]);
-    							
+    							datos[i][j]=null;								
     						}
-    						
-    						//System.out.print("\n");
     					}
     					llena = true;
-    					//System.out.print("LLENO");
     				}
     				
     				if(llena==true){
     					for(int i=0;i<cuenta;i++){
-    						for(int j = 0;j<3;j++){
+    						for(int j = 0;j<columnas;j++){
     							datos[i][j]=null;
     						}
     					}
-    					//System.out.print("LIMPIADO");
-    					
     					if(jtxt.getText().equals("")){
-    						
     						for(int i=0;i<cuenta;i++){
-        						for(int j = 0;j<3;j++){
+        						for(int j = 0;j<columnas;j++){
         							datos[i][j]=auxdatos[i][j];
         						}
         					}
     				    	conexion.cerrarConexion();
     				    	tablemodel.setDataVector(datos, colu);
-    				    	//System.out.print("BLANCO");
     					}else{
-    						//System.out.print("MOVIDA");
-    						
     						int tam = jtxt.getText().length();
     						int i=0;
     						int j=0;
     						int fila=0;
-    						String[] res = new String[3];//almacenamos los resultados
+    						String[] res = new String[columnas];//almacenamos los resultados
     						if(tablemodel.getRowCount() != 0){//si la tabla no esta vacia la vaciamos
     							for( int a = tablemodel.getRowCount() - 1; a >= 0; a-- ){
     						tablemodel.removeRow(a);
     							}
     						}
-    						
-    						
     						try{
-    							//System.out.print("\n");
-								//System.out.print("\n");
-	    						while(auxdatos[i][j]!=null){
-	    							while(auxdatos[i][j]!=null){
-	    								//System.out.print("llega");
-	    								
+	    						//while(auxdatos[i][j]!=null){
+	    						for(j=0;j<3;j++){
+	    							//while(auxdatos[i][j]!=null){
+	    							for(i=0;i<cuenta;i++){
 	    								if(auxdatos[i][j]!= null){
 		    								if(auxdatos[i][j].regionMatches( true, 0, jtxt.getText(), 0, tam )){
-		    									//System.out.print("ENTRÓO");
-		    									
-		    									for(int col =0;col<3;col++){
-		    										
-		    										datos[fila][col]= auxdatos[i][col];
-		    										
-		    										res[col] = datos[fila][col];
+		    									existe=buscar_reg(datos,auxdatos,i,cuenta,columnas);
+		    									if(existe==false){
+			    									for(int col =0;col<columnas;col++){
+			    										datos[fila][col]= auxdatos[i][col];
+			    										res[col] = datos[fila][col];
+			    									}
+			    									fila++;
+			    									
+			    									Object[] dat = {res[0],res[1],res[2]};
+			    									tablemodel.addRow(dat);
 		    									}
-		    									fila++;
-		    									
-		    									Object[] dat = {res[0],res[1],res[2]};
-		    									tablemodel.addRow(dat);
-		    									
 		    									
 		    								}
 	    								}
 	    								
-	    								i++;
+	    								//i++;
 	    							}
-	    							i=0;
-	    							j++;
+	    							//i=0;
+	    							//j++;
 	    						}
 	    						
     						}catch(ArrayIndexOutOfBoundsException act1) {
-    						         //System.out.println("This program takes 3 parameters: ");
-    						         //System.out.println("  month day year.");
     						}
     					}
     						
@@ -387,7 +380,16 @@ public class PnlModificarsocio extends JPanel{
 					
 					if(e.getActionCommand().equals("eliminar")){
 						conexion.Conectardb();
-						conexion.executeUpdate("DELETE FROM STAFF WHERE nombre LIKE '"+datos[jtblLateral.getSelectedRow()][0]+"'");
+						conexion.executeUpdate("DELETE FROM PARTNER WHERE nombre LIKE '"+datos[jtblLateral.getSelectedRow()][0]+"'");
+						
+						cuenta=contar_reg();
+						datos = new String[cuenta][columnas];
+						auxdatos = new String[cuenta][columnas];
+						tablemodel =cargar_tabla(datos);
+						jtblLateral.setModel(tablemodel);
+						llena = false;
+						//jtblLateral.repaint();
+						//jtblLateral.revalidate();
 						Component aviso = null;
 						JOptionPane.showMessageDialog(aviso, rec.idioma[rec.eleidioma][63]);
 						conexion.cerrarConexion();
@@ -416,4 +418,81 @@ public class PnlModificarsocio extends JPanel{
 		   
     
     }
+    
+    
+    
+    public DefaultTableModel cargar_tabla(String datos[][]){
+    	
+    	String resul[]= new String[3];
+    	conexion.Conectardb();
+    	//rs = conexion.ConsultaSQL("SELECT COUNT(*) FROM STAFF");
+    	
+		//datos = new String[cuenta][3];
+		//auxdatos = new String[cuenta][3];
+		
+    	
+    	rs = conexion.ConsultaSQL("SELECT p.nombre,p.telefono,p.telefono2 FROM PARTNER p");
+    	int i=0;
+    	try {
+			while(rs.next()){
+				for(int j = 1;j<4;j++){
+					datos[i][j-1] = rs.getString(j);
+					fila[j-1]=datos[i][j-1];
+					System.out.print(fila[j-1]+";");
+				}
+				System.out.print("\n");
+				i++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	conexion.cerrarConexion();
+    	
+    	DefaultTableModel tablemodel= new DefaultTableModel(datos,colu);
+    	return tablemodel;
+    }
+    
+    public int contar_reg(){
+		
+    	conexion.Conectardb();
+    	//rs = conexion.ConsultaSQL("SELECT COUNT(*) FROM STAFF");
+    	rs = conexion.ConsultaSQL("SELECT p.nombre,p.telefono,p.telefono2 FROM PARTNER p");
+    	cuenta = 0;
+    	try {
+			while(rs.next()){
+				cuenta++;
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		conexion.cerrarConexion();
+    	return cuenta;
+    	
+    }
+    
+    public Boolean buscar_reg(String datos[][],String auxdatos[][],int i,int cuenta,int columnas){
+		
+    	Boolean existe = false;
+    	int cont=0;
+    	for(int f =0;f<cuenta;f++){
+    		for(int c = 0;c<columnas;c++){
+    			if(auxdatos[i][c].equals(datos[f][c])){
+    				cont++;
+    			}
+    		}
+    		if(cont==columnas){
+    			existe=true;
+    		}
+    		cont=0;
+    	}
+    	
+    	return existe;
+    }
+    
+    public static PnlModificarsocio Obtener_Instancia(){
+		return instancia;
+	}
 }
