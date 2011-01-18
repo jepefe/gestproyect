806ea51d9272b76ab4945ec0576cf0989b86c7df
bernabe.r.v@gmail.com <bernabe.r.v@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-18 17:54:34 +0000

    Usuario y contraseña

--- GesProject/src/pkGesproject/FrmLogin.java
+++ GesProject/src/pkGesproject/FrmLogin.java
@@ -24,8 +24,8 @@ public class FrmLogin extends JFrame implements ActionListener{
 	GesIdioma rec = GesIdioma.obtener_instancia();
 	JButton jbtnaceptar = new JButton(rec.idioma[rec.eleidioma][1]);
 	JButton jbtncancelar = new JButton(rec.idioma[rec.eleidioma][2]);
-	JTextField jtxfUsuario = new JTextField();
-	JPasswordField jpwfPassword = new JPasswordField();
+	JTextField jtxfUsuario = new JTextField("floridae");
+	JPasswordField jpwfPassword = new JPasswordField("123");
 	GridBagConstraints cons = new GridBagConstraints();
 	JLabel jlblUsuario = new JLabel(rec.idioma[rec.eleidioma][10]);
 	JLabel jlblPassword = new JLabel(rec.idioma[rec.eleidioma][11]);
