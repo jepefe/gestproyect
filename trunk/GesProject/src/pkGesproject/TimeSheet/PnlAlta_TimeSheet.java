bb44dd49b6cf9faa8685ff7a5fbe9a2bcdacdf6f
veesum@gmail.com <veesum@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-18 12:38:42 +0000

    Alta_Timesheet modificado

--- trunk/GesProject/src/pkGesproject/TimeSheet/PnlAlta_TimeSheet.java
+++ trunk/GesProject/src/pkGesproject/TimeSheet/PnlAlta_TimeSheet.java
@@ -4,35 +4,42 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.toedter.calendar.JDateChooser;
 
 import pkGesproject.GesIdioma;
+import pkGesproject.LimiteDocumento;
 import pkGesproject.RsGesproject;
 
 public class PnlAlta_TimeSheet extends JPanel{
 	
+	GesIdioma rec = GesIdioma.obtener_instancia();
+	RsGesproject recursos = RsGesproject.Obtener_Instancia();
 	JTextField[] jtxt;
 	JLabel[] jlbl;
 	JDateChooser jdc1;
 	int i;
 	JLabel jlblfecha;
-	GesIdioma rec = GesIdioma.obtener_instancia();
-	RsGesproject recursos = RsGesproject.Obtener_Instancia();
+	JTextArea textarea = (new JTextArea(3,13));
+	JButton jbtnaceptar, jbtncancelar;
+	
 	
 	public PnlAlta_TimeSheet(){
 		this.setLayout(new GridBagLayout());
 		
 		String[] fieldNames = {
-				rec.idioma[rec.eleidioma][96],
+				
 				rec.idioma[rec.eleidioma][97]
 
 		};
 		
-		int[] fieldWidths = {10,30,6};
+		int[] fieldWidths = {7};
 		jtxt = new JTextField[fieldNames.length];
 		jlbl = new JLabel[fieldNames.length];
 		
@@ -52,8 +59,36 @@ public class PnlAlta_TimeSheet extends JPanel{
 				gbc.anchor = GridBagConstraints.WEST;
 				gbc.gridwidth = GridBagConstraints.REMAINDER;
 				this.add(jdc1,gbc);
+				
+				gbc.gridwidth = GridBagConstraints.RELATIVE;
+		    	this.add(new JLabel(rec.idioma[rec.eleidioma][96]),gbc); 
+		    	gbc.gridwidth = GridBagConstraints.REMAINDER;
+		    
+		    	LimiteDocumento lpd = new LimiteDocumento(200); // Limite JTextArea
+		    	
+		    	textarea.setDocument(lpd);
+		    	textarea.setLineWrap(true);
+		    	JScrollPane sp = new JScrollPane(textarea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		    	JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		    	this.add((sp),gbc);
+				
 			}
+			
+			gbc.gridwidth = GridBagConstraints.RELATIVE;
+			gbc.anchor = GridBagConstraints.EAST;
+			this.add(jlbl[i]=new JLabel(fieldNames[i]),gbc);
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.gridwidth = GridBagConstraints.REMAINDER;
+			this.add(jtxt[i]=new JTextField(fieldWidths[i]),gbc);
+			
 		}
+	    gbc.anchor = GridBagConstraints.EAST;
+		gbc.insets = new Insets(30,10,5,5);
+		gbc.gridwidth = GridBagConstraints.RELATIVE;
+		this.add(jbtnaceptar=new JButton(rec.idioma[rec.eleidioma][1]),gbc);
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(jbtncancelar=new JButton(rec.idioma[rec.eleidioma][2]),gbc);
 	    this.setVisible(true);
 	}
 	
