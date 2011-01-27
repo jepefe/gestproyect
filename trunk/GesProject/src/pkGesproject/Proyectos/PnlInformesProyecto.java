93331c37efbc964222771d3e7fb90263fab7dedd
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-01-27 14:19:58 +0000

    Ya se genera informes de prueba

--- trunk/GesProject/src/pkGesproject/Proyectos/PnlInformesProyecto.java
+++ trunk/GesProject/src/pkGesproject/Proyectos/PnlInformesProyecto.java
@@ -3,15 +3,23 @@ package pkGesproject.Proyectos;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+import pkGesproject.ConexionDb;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -26,7 +34,13 @@ public class PnlInformesProyecto extends JPanel{
 	JasperReport jasperReport;
 	JasperPrint jasperPrint;
 	String fileName;
+	ResultSet rs;
+	ConexionDb conexion= new ConexionDb();
+	JFileChooser filechooser;
+	String ruta;
+	
 	public PnlInformesProyecto(){
+		
 		this.setLayout(new GridBagLayout());
 		this.add(btngenerar);
 		fileName = System.getProperty("user.dir")+"//src//pkGesproject//reports//prueba.jrxml";
@@ -37,29 +51,41 @@ public class PnlInformesProyecto extends JPanel{
 				// TODO Auto-generated method stub
 				try
 				{
+					
+					//Hacemos las consulta y la guardamos en el resulset
+					String consulta = "SELECT *,(SELECT COUNT(*) FROM IDIOMA) AS total FROM IDIOMA";
+					conexion.Conectardb();
+					rs = conexion.ConsultaSQL(consulta);
+					JRResultSetDataSource resulset = new JRResultSetDataSource(rs);
+					
 					//1-Compilamos el archivo XML y lo cargamos en memoria
 					jasperReport = JasperCompileManager.compileReport(fileName);
+					
 
 					//2. Se llena el reporte con la informacion necesaria
-					/*Map pars = new HashMap();
-				pars.put("P_INSTITUCION", "Universidad de Montemorelos");
-				pars.put("P_FACULTAD", "Facultad de Ingeniería Tecnología");
-				pars.put("P_CARRERA", "Ingeniería en Sistemas Computacionales");
-
-				jasperPrint = JasperFillManager.fillReport(jasperReport, pars, new JREmptyDataSource());
-					 */
-					jasperPrint = JasperFillManager.fillReport(
-							jasperReport, new HashMap(), new JREmptyDataSource());
-
-
-
+					jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap(), resulset);
+					
+					conexion.cerrarConexion();
 					//3-Exportamos el reporte a pdf y lo guardamos en disco
+					filechooser = new JFileChooser();
+					int returnVal = filechooser.showSaveDialog(null);
+					//FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, GIF, PNG", "jpg", "jpeg", "gif", "png");//Añadimos el filtro para que nos muestre sólo las extensiones que queremos
+					//filechooser.setFileFilter(filter);
+					if (returnVal == JFileChooser.APPROVE_OPTION) {
+						File file = filechooser.getSelectedFile();
+						/*
+						 * sacamos la ruta del archivo y su extension
+						 */
+						ruta = file.getPath();
+				    } 	
 					JasperExportManager.exportReportToPdfFile(
-							jasperPrint, "/Users/ruben_albi/Desktop/pruebas/reporte.pdf");
+							jasperPrint, ruta+".pdf");
+					System.out.println("SE A CREADO CORRECTAMENTE EL PDF");
 				}
 				catch (JRException e)
 				{
 					e.printStackTrace();
+					System.out.println("ERROR EL PDF NO SE A CREADO!!!");
 				}
 			}
 
