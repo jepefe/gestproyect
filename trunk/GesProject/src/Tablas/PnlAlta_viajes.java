2f8b694d481970718608d3448d64f4741470d584
soriaromanm@gmail.com <soriaromanm@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-02-25 18:53:34 +0000

    Funcionamiento adecuado de limpiar campos en Alta_viajes, y alta casi completa

--- trunk/GesProject/src/Tablas/PnlAlta_viajes.java
+++ trunk/GesProject/src/Tablas/PnlAlta_viajes.java
@@ -33,10 +33,11 @@ public class PnlAlta_viajes extends JPanel{
 	JButton jbtnaceptar, jbtnlimpiar;
 	GpComboBox cbpartner,cbproyecto,cbcsalida,cbpsalida,cbcdestino,cbpdestino;
 	ConexionDb conexion= new ConexionDb();
-	ResultSet rs,rs2;
+	ResultSet rs1,rs2,rs3,rs4,rs;
 	JDateChooser jdc1,jdc2;
 	Component aviso;
 	int i;
+	boolean OKalta;
 	
 	public PnlAlta_viajes(){
 		this.setLayout(new GridBagLayout());
@@ -93,6 +94,7 @@ public class PnlAlta_viajes extends JPanel{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				cbpartner.setSelectedItem(null);//inicializamos en blanco
 				
 				//proyecto cmbbox
 				gbc.gridwidth = GridBagConstraints.RELATIVE;
@@ -112,6 +114,7 @@ public class PnlAlta_viajes extends JPanel{
 					e.printStackTrace();
 				}
 				cbproyecto.setPreferredSize(new Dimension(233,30));
+				
 			}
 			
 			if(i==2){
@@ -127,10 +130,13 @@ public class PnlAlta_viajes extends JPanel{
 						cbpsalida.addItem(rs.getString(1));
 					
 					}
+					cbpsalida.setSelectedItem(null);//iniciamos en blanco
+					
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
 				
 				gbc.gridwidth = GridBagConstraints.RELATIVE;
 				gbc.anchor = GridBagConstraints.EAST;
@@ -153,6 +159,7 @@ public class PnlAlta_viajes extends JPanel{
 						cbpdestino.addItem(rs.getString(1));
 					
 					}
+					cbpdestino.setSelectedItem(null);
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -209,163 +216,227 @@ public class PnlAlta_viajes extends JPanel{
 				// TODO Auto-generated method stub
 				if(e.getActionCommand().equals("cbsalida")){
 					int pais = 0;
-					rs = conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpsalida.getSelectedItem().toString()+"'");
+					if(cbpsalida.getSelectedItem()!=null){
+						rs = conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpsalida.getSelectedItem().toString()+"'");
 					
-					try {
-						rs.next();
-						pais = rs.getInt(1);
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+						try {
+							rs.next();
+							pais = rs.getInt(1);
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					
-					rs = conexion.ConsultaSQL("SELECT estado FROM PROVINCIAS WHERE id_pais = "+pais);
-					int j = 0 ;
-					cbcsalida.removeAllItems(); 
-					try {
-						while(rs.next()){	
-								
+						rs = conexion.ConsultaSQL("SELECT estado FROM PROVINCIAS WHERE id_pais = "+pais);
+						int j = 0 ;
+						cbcsalida.removeAllItems(); 
+						try {
+							while(rs.next()){	
 								cbcsalida.addItem(rs.getString(1));
 								j++;
+							}
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
 						}
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
 					}
 				}
 				
 				if(e.getActionCommand().equals("cbdestino")){
 					int pais = 0;
-					rs = conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpdestino.getSelectedItem().toString()+"'");
+					if(cbpdestino.getSelectedItem()!=null){	
+						rs = conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpdestino.getSelectedItem().toString()+"'");
 					
-					try {
-						rs.next();
-						pais = rs.getInt(1);
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+						try {
+							rs.next();
+							pais = rs.getInt(1);
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					
-					rs = conexion.ConsultaSQL("SELECT estado FROM PROVINCIAS WHERE id_pais = "+pais);
-					int j = 0 ;
-					cbcdestino.removeAllItems(); 
-					try {
-						while(rs.next()){	
+						rs = conexion.ConsultaSQL("SELECT estado FROM PROVINCIAS WHERE id_pais = "+pais);
+						int j = 0 ;
+						cbcdestino.removeAllItems(); 
+						try {
+							while(rs.next()){	
 								
 								cbcdestino.addItem(rs.getString(1));
 								j++;
+							}
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
 						}
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
 					}
 				}
 				//
 				if(e.getActionCommand().equals("cbpartner")){
 					int part = 0;
-					rs = conexion.ConsultaSQL("SELECT cod_part FROM PARTNER WHERE nombre like '"+cbpartner.getSelectedItem().toString()+"'");
+					if (cbpartner.getSelectedItem()!=null){
+						rs = conexion.ConsultaSQL("SELECT cod_part FROM PARTNER WHERE nombre like '"+cbpartner.getSelectedItem().toString()+"'");
 					
-					try {
-						rs.next();
-						part = rs.getInt(1);
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+						try {
+							rs.next();
+							part = rs.getInt(1);
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					
-					rs = conexion.ConsultaSQL("SELECT PROYECTOS.nombre FROM PROYECTOS INNER JOIN PARTNER_PROYECTOS ON PROYECTOS.id_pro = PARTNER_PROYECTOS.id_pro WHERE cod_part = "+part);
-					int j = 0 ;
-					cbproyecto.removeAllItems(); 
-					try {
-						while(rs.next()){	
+						rs = conexion.ConsultaSQL("SELECT PROYECTOS.nombre FROM PROYECTOS INNER JOIN PARTNER_PROYECTOS ON PROYECTOS.id_pro = PARTNER_PROYECTOS.id_pro WHERE cod_part = "+part);
+						int j = 0 ;
+						cbproyecto.removeAllItems(); 
+						try {
+							while(rs.next()){	
 								
 								cbproyecto.addItem(rs.getString(1));
 								j++;
+							}
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
 						}
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
 					}
 				}
-				//
+				
+				if(e.getActionCommand().equals("limpiar")){
+				//vaciamos todos los txtboxs
+					jtxt[0].setText(null);
+					jtxt[1].setText(null);
+					jtxt[2].setText(null);
+					jtxt[3].setText(null);
+				//vaciamos fechas
+					jdc1.setDate(null);
+					jdc2.setDate(null);
+				//vaciamos comboboxs relacionados
+					cbcdestino.removeAllItems(); 
+					cbcsalida.removeAllItems();
+					cbproyecto.removeAllItems(); 
+				//ponemos a null los comboboxs principales
+					cbpartner.setSelectedItem(null);
+					cbpdestino.setSelectedItem(null);
+					cbpsalida.setSelectedItem(null);
+				}
+				//realizamos el alta
 				if(e.getActionCommand().equals("aceptar")){
-					System.out.print("entra");
-					if(jtxt[0].equals("")||jtxt[1].equals("")||jtxt[2].equals("")||jtxt[3].equals("")){
-						JOptionPane.showMessageDialog(aviso,"Revisa los campos obligatorios");
-					}else{
-						
+					System.out.println("entra");
+					comprobar_campos();//comprobamos la validez de los datos introducidos
+					if(OKalta = true){
+						//guardamos el codigo del partner
 						rs= conexion.ConsultaSQL("SELECT cod_part FROM PARTNER WHERE nombre like '"+cbpartner.getSelectedItem().toString()+"'");
-						
 						String partner = null;
 						try {
 							rs.next();
 							partner = rs.getString(1);
 						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
+						// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-								rs= conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpsalida.getSelectedItem().toString()+"'");
-								
-								String paissalida = null;
-							try {
-								rs.next();
-								paissalida = rs.getString(1);
-							} catch (SQLException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-							rs = null;
-							rs= conexion.ConsultaSQL("SELECT id_provincias FROM PROVINCIAS WHERE estado like '"+cbcsalida.getSelectedItem().toString()+"'");
-							
-							String ciudadsalida = null;
+
+						//guardamos el codigo del proyecto
+						rs = null;
+						rs = conexion.ConsultaSQL("SELECT id_pro FROM PROYECTOS WHERE nombre like '"+cbproyecto.getSelectedItem().toString()+"'");
+						String proyecto = null;
 						try {
 							rs.next();
-							ciudadsalida = rs.getString(1);
+							proyecto = rs.getString(1);
 						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
+						// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+		
+						//guardamos el id del pais de origen
+						rs = null;
+						rs = conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpsalida.getSelectedItem().toString()+"'");
+						String paissalida = null;
+						try {
+							rs.next();
+							paissalida = rs.getString(1);
+						} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+
+						//guardamos el id de la provincia de salida
+						rs = null;
+						rs = conexion.ConsultaSQL("SELECT id_provincias FROM PROVINCIAS WHERE estado like '"+cbcsalida.getSelectedItem().toString()+"'");
+						String prosalida = null;
+						try {
+							rs.next();
+							prosalida = rs.getString(1);
+						} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+	
+						//guardamos el id del pais de destino
 						rs = null;
 						rs= conexion.ConsultaSQL("SELECT id_pais FROM PAIS WHERE pais like '"+cbpdestino.getSelectedItem().toString()+"'");
-						
-							String paisdestino = null;
+						String paisdestino = null;
 						try {
 							rs.next();
 							paisdestino = rs.getString(1);
 						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
+						// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+					
+						//guardamos el id de la provincia de destino
 						rs = null;
 						rs= conexion.ConsultaSQL("SELECT id_provincias FROM PROVINCIAS WHERE estado like '"+cbcdestino.getSelectedItem().toString()+"'");
-							
-							String ciudaddestino = null;
+						String prodestino = null;
 						try {
 							rs.next();
-							ciudaddestino = rs.getString(1);
+							prodestino = rs.getString(1);
 						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
+						// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+						//cambiamos fechas a sql
+						java.sql.Date sqlDate1 = new java.sql.Date(jdc1.getDate().getTime());
+						java.sql.Date sqlDate2 = new java.sql.Date(jdc2.getDate().getTime());
+					
+						
+						conexion.executeUpdate("INSERT INTO TRAVEL_SUBSISTENCE (partner,nombrepersona,motivoviaje,paissalida,ciudad_salida,paisdestino,ciudad_destino,fecha_ini,fecha_fin,coste_viaje,coste_subsistencia,id_pro) VALUES ('"
+								+partner+"','"+jtxt[0].getText()+"','"+jtxt[1].getText()+"','"+paissalida+"','"+prosalida+"','"+paisdestino+"','"+prodestino+"','"+sqlDate1+"','"+sqlDate2+"','"+jtxt[2].getText()+"','"+jtxt[3].getText()+"')");
+							//JOptionPane.showMessageDialog(aviso,"Subido");
 				
-						conexion.executeUpdate("INSERT INTO TRAVEL_SUBSISTENCE (partner,nombrepersona,motivoviaje,paissalida,ciudad_salida,paisdestino,ciudad_destino,fecha_ini,fecha_fin,coste_viaje,coste_subsistencia,gastos_totales) VALUES" +
-								"("+partner+",'"+jtxt[0].getText()+"','"+jtxt[1].getText()+"',"+paissalida+","+ciudadsalida+","+paisdestino+","+ciudaddestino+",'"+jdc1.getDate()+"','"+jdc2.getDate()+"','"+jtxt[2].getText()+"','"+jtxt[3].getText()+"')");
-						JOptionPane.showMessageDialog(aviso,"Subido");
-				
+						
 					}
 				}
 			}
-			
 		};
 		
 		cbpartner.addActionListener(accion);
 		cbpsalida.addActionListener(accion);
 		cbpdestino.addActionListener(accion);
 		jbtnaceptar.addActionListener(accion);
+		jbtnlimpiar.addActionListener (accion);
 		cbpartner.setActionCommand("cbpartner");
 		cbpsalida.setActionCommand("cbsalida");
 		cbpdestino.setActionCommand("cbdestino");
 		jbtnaceptar.setActionCommand("aceptar");
+		jbtnlimpiar.setActionCommand("limpiar");
 		this.setVisible(true);
+	}
+	//nos aseguramos de que los campos sean validos
+	public void comprobar_campos(){
+		/*
+		 * Revisamos si estan rellenados todos los camps obligatorios
+		 */
+				
+		OKalta = true;
+		for(int Y=0;Y<4;++Y) {
+			if (jtxt[Y].getText().length() > 0){
+			}else{
+				OKalta = false;				
+			}	
+		}
+		if((cbpartner.getSelectedItem() != null) && (cbproyecto.getSelectedItem() != null) && (cbpsalida.getSelectedItem() != null)&& (cbcsalida.getSelectedItem() != null)&& (cbpdestino.getSelectedItem() != null)&& (cbcdestino.getSelectedItem() != null)){	
+			
+		}else{
+			OKalta = false;
+		}	
 	}
 }
