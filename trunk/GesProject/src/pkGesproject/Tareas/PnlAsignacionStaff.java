a3f0bfac67c6026725967d871f70fe459b0c0cd8
ruben.albiach@gmail.com <ruben.albiach@gmail.com@46d8371f-59be-29b9-8b59-188e4420dafa>
2011-03-09 18:44:51 +0000

    Creado panel asignacion tareas

--- trunk/GesProject/src/pkGesproject/Tareas/PnlAsignacionStaff.java
+++ trunk/GesProject/src/pkGesproject/Tareas/PnlAsignacionStaff.java
@@ -83,6 +83,7 @@ public class PnlAsignacionStaff extends JPanel{
 
 	GpComboBox cmbProyectoyecto,cmbWp,cmbTasks,cmbStaff;
 
+	RsGesproject recursos = RsGesproject.Obtener_Instancia();
 	GesIdioma rec = GesIdioma.obtener_instancia();
 	//9637
 
@@ -90,22 +91,22 @@ public class PnlAsignacionStaff extends JPanel{
 	JLabel[] jlbl,jlblWp,jlblTasks;
 	String id_p = null;
 	ResultSet rp;
-	String[] projects = new String[200];
+	int[] projects = new int[200];
 	ActionListener asignacion;
 	ActionListener importar;
 	JFileChooser filechooser;
 	ConexionFTP cftp=new ConexionFTP();
 	ConexionDb conexion= new ConexionDb();
 	HSSFWorkbook tuWorkBook = new HSSFWorkbook();
-	JButton btnAsignar;
+	JButton btnAsignar,btnQuitar;
 	GpComboBox cmbProyecto = new GpComboBox();
 	GridBagConstraints gbc;
-	String[] columnstaff = {"Staff","Apellidos"},columnrolstaff = {"Staff","Categoria"};
+	String[] columnstaff = {"Nombre","Apellidos"},columnrolstaff = {"Nombre","Apellidos","Categoria"};
 	String[][] staff,rolstaff;
 	JTable jtblstaff = new JTable(),jtblrolstaff= new JTable();
 	DefaultTableModel modelstaff = new DefaultTableModel(null,columnstaff);
 	DefaultTableModel modelcate = new DefaultTableModel(null,columnrolstaff);
-	JScrollPane tablastaff,tabla;
+	JScrollPane tablastaff,tablarolstaff;
 	int regstaff=0;
 	public PnlAsignacionStaff(){
 
@@ -117,14 +118,13 @@ public class PnlAsignacionStaff extends JPanel{
 		asignacion = new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
+				cargar_tabla();
 			}
 
 		};
 
 		
-		btnAsignar.setActionCommand("Asignar");
-		btnAsignar.addActionListener(asignacion);
+		cmbProyecto.addActionListener(asignacion);
 		this.setVisible(true);
 	}
 
@@ -148,55 +148,93 @@ public class PnlAsignacionStaff extends JPanel{
 		cmbProyecto.setPreferredSize(new Dimension(140,30));
 
 		
-		gbc.gridx = 2; // El área de texto empieza en la columna
+		gbc.gridx = 3; // El área de texto empieza en la columna
 		jlblWp = new JLabel[fieldNames.length];
 		this.add(jlblWp[i]=new JLabel("Work Packages"),gbc);
-		gbc.gridx = 3; // El área de texto empieza en la columna
+		gbc.gridx = 4; // El área de texto empieza en la columna
 		this.add(cmbStaff= new GpComboBox(),gbc);
 		cmbStaff.setPreferredSize(new Dimension(140,30));
 
-		gbc.gridx = 4; // El área de texto empieza en la columna
+		gbc.gridx = 5; // El área de texto empieza en la columna
 		jlblTasks = new JLabel[fieldNames.length];
 		this.add(jlblTasks[i]=new JLabel("Tareas"),gbc);
-		gbc.gridx = 5; // El área de texto empieza en la columna
+		gbc.gridx = 6; // El área de texto empieza en la columna
 		this.add(cmbTasks= new GpComboBox(),gbc);
 		cmbTasks.setPreferredSize(new Dimension(140,30));
 		
-		gbc.gridx = 6; // El área de texto empieza en la columna
-		this.add(btnAsignar = new JButton("Asignar"),gbc);
+		gbc.gridx = 2; // El área de texto empieza en la columna
+		gbc.gridy = 1; // El área de texto empieza en la fila
+		gbc.gridwidth = 1; // El área de texto ocupa x columnas.
+		gbc.gridheight = 1; // El área de texto ocupa x filas.
+		gbc.anchor = GridBagConstraints.SOUTH; 
+		gbc.insets = new Insets(130,0,0,0);
+		this.add(btnAsignar = new JButton(">"),gbc);
+		gbc.insets = new Insets(0,0,0,0);
 		
+		gbc.gridx = 2; // El área de texto empieza en la columna
+		gbc.gridy = 2; // El área de texto empieza en la fila
+		gbc.gridwidth = 1; // El área de texto ocupa x columnas.
+		gbc.gridheight = 1; // El área de texto ocupa x filas.
+		gbc.anchor = GridBagConstraints.NORTH; 
+		this.add(btnQuitar = new JButton("<"),gbc);
 		
 		gbc.gridx = 0; // El área de texto empieza en la columna
 		gbc.gridy = 1; // El área de texto empieza en la fila
 		gbc.gridwidth = 2; // El área de texto ocupa x columnas.
-		gbc.gridheight = 1; // El área de texto ocupa x filas.
+		gbc.gridheight = 2; // El área de texto ocupa x filas.
+		gbc.anchor = GridBagConstraints.CENTER; 
 		this.add(tablastaff = new JScrollPane(jtblstaff),gbc);
+		
+		gbc.gridx = 3; // El área de texto empieza en la columna
+		gbc.gridwidth = 4; // El área de texto ocupa x columnas.
+		gbc.anchor = GridBagConstraints.CENTER; 
+		this.add(tablarolstaff = new JScrollPane(jtblrolstaff),gbc);
 		
 		conexion.Conectardb();
 		rp = conexion.ConsultaSQL("SELECT nombre,id_pro FROM PROYECTOS ORDER BY nombre");
-
+		
 		int j=0;
 		try {
 
 			while(rp.next()){
 
-				projects[j]= Integer.toString(rp.getInt(2));
+				projects[j]= rp.getInt(2);
 
 				cmbProyecto.addItem(rp.getString(1));
 				j++;
-				cmbProyecto.setSelectedItem(null);
-
 			}
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		cmbProyecto.setSelectedItem(null);
 
 	}
 	
 	public void crear_tabla(){
+		
+		modelstaff = new DefaultTableModel(staff,columnstaff);
+		jtblstaff = new JTable(modelstaff){
+	   		 public boolean isCellEditable(int rowIndex, int mColIndex) {
+	                return false;
+	              }
+	    };
+		
+	    jtblstaff.setPreferredScrollableViewportSize(new Dimension(220,300));
+	    
+	    
+	    modelcate = new DefaultTableModel(rolstaff,columnrolstaff);
+	    jtblrolstaff = new JTable(modelcate){
+	   		 public boolean isCellEditable(int rowIndex, int mColIndex) {
+	                return false;
+	              }
+	    };
+	    jtblrolstaff.setPreferredScrollableViewportSize(new Dimension(420,300));
+	}
+	
+	public void cargar_tabla(){
 		conexion.Conectardb();
-		rp = conexion.ConsultaSQL("SELECT COUNT(*) FROM STAFF ORDER BY nombre");
+		rp = conexion.ConsultaSQL("SELECT COUNT(*) FROM STAFF WHERE cod_part = "+recursos.getCodparter()+" ORDER BY nombre");
 		try {
 			rp.next();
 			regstaff = rp.getInt(1);
@@ -206,7 +244,7 @@ public class PnlAsignacionStaff extends JPanel{
 			e.printStackTrace();
 		}
 		
-		rp = conexion.ConsultaSQL("SELECT nombre,apellidos FROM STAFF ORDER BY nombre");
+		rp = conexion.ConsultaSQL("SELECT nombre,apellidos FROM STAFF WHERE cod_part = "+recursos.getCodparter()+" ORDER BY nombre");
 		for(int i =0;i< regstaff;i++){
 			try {
 				rp.next();
@@ -223,8 +261,6 @@ public class PnlAsignacionStaff extends JPanel{
 		
 		modelstaff = new DefaultTableModel(staff,columnstaff);
 		jtblstaff.setModel(modelstaff);
-		
-		
 	}
 
 }
